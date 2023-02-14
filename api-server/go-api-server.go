0dffc72cf85fc70c55e175adf9a901a587e7d56d
Fernando Pérez <jumbonionga+github@gmail.com>
2023-02-13 19:02:16 -0600

    Organizing and renaming. Updating gitignore

--- api-server/go-api-server.go
+++ api-server/go-api-server.go
@@ -16,8 +16,8 @@ func main() {
 
 	router.Get("/api/example/", handlers.GetTest)
 	router.Post("/api/example/", handlers.PostTest)
-	router.Post("/api/search/*", handlers.Search)
+	router.Get("/api/search/*", handlers.Search)
 
-	fmt.Printf("Listening on port %d", port)
+	fmt.Printf("Listening on port %d\n", port)
 	log.Fatal(http.ListenAndServe(":"+strconv.FormatInt(int64(port), 10), router))
 }
